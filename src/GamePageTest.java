94d80d5a0537fb6733e11f68dce3e2a12535b6dd
CH Li <cantilena@sina.com>
2020-03-31 20:51:33 -0400

    Junit
    this is the juint test for all java file, we can add more untile we actually implemented something

---
+++ src/GamePageTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GamePageTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
---
+++ src/ProfileTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProfileTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
