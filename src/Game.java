48481ea278dab9e51267c93c4b7e5de7de810a07
Chandler Davis <davisc10@miamioh.edu>
2020-04-05 15:44:46 -0400

    Added Game class

---
+++ src/Game.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+/**
+ * A class to represent a Game.
+ * @author Chandler Davis (davisc10@miamioh.edu)
+ *
+ */
+public class Game {
+	private final Profile uploader;
+	private double rating;
+	private ArrayList<String> comments;
+	private String description;
+	private ArrayList<String> previews;
+	
+	public Game(Profile uploader, double rating, ArrayList<String> comments, String description, ArrayList<String> previews) {
+		this.uploader = uploader;
+		this.rating = rating;
+		this.comments = new ArrayList<String>(comments);
+		this.description = description;
+		this.previews = new ArrayList<String>(previews);
+	}
+	
+	public Game() {}
+	
+	public Game(Profile uploader, String description, ArrayList<String> previews) {
+		this(uploader, 0.0, null, description, previews);
+	}
+	
+	public boolean comment(Profile commenter, String newComment) {
+		return false;
+	}
+	
+	public boolean delete(Profile commenter, String delComment) {
+		return false;
+	}
+	
+	public boolean editOwnComment(Profile commenter, String oldComment, String newComment) {
+		return false;
+	}
+	
+	public boolean editDesc(Profile user, String newDesc) {
+		return false;
+	}
+	
+	public boolean editPreviews(Profile user, ArrayList<String> newPreviews) {
+		return false;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public ArrayList<String> getComments() {
+		return comments;
+	}
+
+	public void setComments(ArrayList<String> comments) {
+		this.comments = comments;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ArrayList<String> getPreviews() {
+		return previews;
+	}
+
+	public void setPreviews(ArrayList<String> previews) {
+		this.previews = previews;
+	}
+
+	public Profile getUploader() {
+		return uploader;
+	}
+}
