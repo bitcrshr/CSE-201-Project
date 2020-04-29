f0b29886b94a1421c1da0b0fc2bd3052f38d8a35
charles-oleary <59305621+charles-oleary@users.noreply.github.com>
2020-04-29 15:41:22 -0400

    Unit test for DownloadGame - first edit

---
+++ src/DownloadGameTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+// @author - Charles O'Leary
+
+public class DownloadGameTest {
+
+	@Test
+	void testGetLinkFromDatabase() {
+		DownloadGame game = new DownloadGame("www.game.com", "PacMan");
+		assertTrue(game.gameLink.equals("www.game.com"));
+		assertFalse(game.gameLink.equals("www.notTheGame.com"));
+	}
+	
+	@Test
+	void testUpdateLink() {
+		DownloadGame game = new DownloadGame("www.game.com", "PacMan");
+		assertTrue(game.gameLink.equals("www.game.com"));
+		game.updateLink("www.newGame.com");
+		assertFalse(game.gameLink.equals("www.game.com"));
+		assertTrue(game.gameLink.equals("www.newGame.com"));
+	}
+	
+	@Test
+	void testValidGame() {
+		//Need to factor the game list into the constructor of the download game
+		//Neither of the variables get used because it isnt called as a second parameter
+		String[] tempGameList = {"Mario", "Call of Duty", "Halo"};
+		DownloadGame game = new DownloadGame("www.game.com", "PacMan");
+		assertFalse(game.validGame(game.gameName));
+		String[] newTempGameList = {"Mario", "PacMan", "Call of Duty", "Halo"};
+		assertTrue(game.validGame(game.gameName));
+	}
+	
+//	@Test - Code still needs to be implemented for finding a game from database
+//	void testGetLinkByName() {
+//		
+//	}
+}
